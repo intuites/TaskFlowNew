1c8dc16aa2e4e210cd344e405716f6da88ebf2a4
intuites <122072432+intuites@users.noreply.github.com>
2026-06-27 00:31:38 +0530

    Add HTTPS request to testHttps.js

---
+++ testHttps.js
@@ -0,0 +1,9 @@
+const https = require("https");
+
+https
+  .get("https://www.google.com", (res) => {
+    console.log("Status:", res.statusCode);
+  })
+  .on("error", (err) => {
+    console.error(err);
+  });
